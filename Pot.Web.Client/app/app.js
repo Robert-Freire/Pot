79d400e9628394263068a59ee68b551b447f5286
Robert-Freire <robert.freire@gmail.com>
2014-12-27 23:00:56 +0100

    setup cliente

---
+++ Pot.Web.Client/app/app.js
@@ -0,0 +1,30 @@
+﻿
+var app = angular.module('PotApp', ['ngRoute', 'LocalStorageModule', 'ui.bootstrap']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+
+});
+
+var serviceBase = 'http://localhost:28611/';
+app.constant('ngAuthSettings', {
+    apiServiceBaseUri: serviceBase,
+    clientId: 'ngTestWebApi'
+});
+
+//app.config(function ($httpProvider) {
+//    $httpProvider.interceptors.push('authInterceptorService');
+//});
+
+app.run(['authService', function (authService) {
+       authService.fillAuthData();
+}]);
+
+
